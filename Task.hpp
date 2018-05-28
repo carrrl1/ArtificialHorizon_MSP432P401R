42412b43c93b776a40cff573d84bc9b8948e2b83
Carlos Eduardo Solano Abarca <charlie@Charlie-Hackintosh-Pro.local>
2018-05-27 21:41:19 -0600

    Added linked task id

--- Task.hpp
+++ Task.hpp
@@ -17,12 +17,14 @@ class Task
 		bool                IsTaskFinished(void){return m_bIsFinished;};
 		uint8_t             GetTaskPriority(void) {return m_u8Priority;};
 		void                SetTaskPriority(uint8_t i_u8NewPriority){m_u8Priority = i_u8NewPriority;};
+		void                SetLinkedTask(uint8_t i_u8LinkedTaskID){m_u8LinkedTaskID = i_u8LinkedTaskID;};
 		static Mailbox * m_pMailbox; // - Pointer to the main Mailbox
 	private:
 	   static uint8_t m_u8NextTaskID;
 	   uint8_t m_u8Priority;
 	protected:
 	   bool m_bIsFinished;
+	   uint8_t m_u8LinkedTaskID;
 };
 
 #endif /* ASK_HPP_ */
